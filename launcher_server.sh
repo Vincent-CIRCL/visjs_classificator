95b90cbb4d6ca410d60e48e5759215a43387b209
Vincent-CIRCL <zettacircl@protonmail.com>
2019-05-29 13:42:44 +0200

    add: [core] Update with common datastructure

---
+++ launcher_server.sh
@@ -0,0 +1,1 @@
+node server.js -i ./../carl-hauser/datasets/MINI_DATASET -t ./TMP -o ./TMP
--- public/js/export_lib.js
+++ public/js/export_lib.js
@@ -13,6 +13,160 @@ function export_classes_json(){
     trigger_file_download(jsonData, 'json');
 }
 
+function export_good_graphe(){
+    var jsonData = graphe_correct_to_json();
+    trigger_file_download(jsonData, 'json');
+}
+
+// ================ LISTS creators ================
+/*
+'nodes': [{'id': '0_0',
+    'image': '',
+    'label': 'picture name +0_0',
+    'shape': 'image'},
+    {'id': '0_1',
+    'image': '',
+    'label': 'picture name +0_1',
+    'shape': 'image'},
+*/
+function export_node(curr_node){
+    tmp_node = {}
+
+    // Add attributes of a node to the new list
+    tmp_node.id = curr_node.id
+    tmp_node.image = curr_node.image
+    if (curr_node.label !== null){
+        tmp_node.label = curr_node.label
+    }
+    tmp_node.shape = curr_node.shape
+    if (curr_node.group !== null){
+        tmp_node.group = curr_node.group
+    }
+    return tmp_node
+}
+
+function is_an_anchor(id){
+    console.log("Checking id ", id)
+    for(var i = 0; i < anchor_list.length; i++) {
+        if(id === anchor_list[i]){
+            console.log("Found as anchor ", id)
+            return true
+        }
+    }
+    console.log("Not an anchor ", id)
+    return false
+}
+
+function get_nodes(with_cluster=true){
+    var nodeData = [];
+
+    if(with_cluster){
+        console.log("Getting nodes, with clusters")
+
+        // Add nodes with clusters (nothing special)
+        data.nodes.get().forEach(function(nodeD) {
+            // Store the node in the list to be exported
+            nodeData.push(export_node(nodeD));
+        });
+    } else {
+        console.log("Getting nodes, without clusters")
+
+        // Add nodes without clusters (jump over clusters)
+        data.nodes.get().forEach(function(nodeD) {
+            if(!is_an_anchor(nodeD.id)){
+                // Store the node in the list to be exported
+                nodeData.push(export_node(nodeD));
+            }
+        });
+    }
+
+    return nodeData
+}
+
+/*
+'edges': [{'color': 'gray', 'from': 0, 'to': '0_0'},
+    {'color': 'gray', 'from': 0, 'to': '0_1'},
+    {'color': 'gray', 'from': 0, 'to': '0_2'},
+*/
+function get_edges(){
+        var edgeData = [];
+
+        // Add edges
+        data.edges.get().forEach(function(edge) {
+            tmp_edge = {}
+
+            // Add attributes of a node to the new list
+            tmp_edge.to = edge.to
+            tmp_edge.from = edge.from
+            //TODO : Color ?
+            // Store the node in the list to be exported
+			edgeData.push(tmp_edge);
+			});
+
+		return edgeData
+}
+
+/*
+{'clusters': [{'group': '',
+    'id': 0,
+    'image': '',
+    'label': '',
+    'members': {'0_0', '0_1', '0_2'},
+    'shape': 'image'},
+    {'group': '',
+    'id': 1,
+    'image': '',
+    'label': '',
+    'members': {'1_0', '1_1', '1_2'},
+    'shape': 'image'}],
+*/
+function get_clusters(){
+    // Construct a json representation of nodes and clusters
+    var clusters = [];
+
+    // Get list of all nodes (by their ids)
+    var node_id_list = data.nodes.getIds()
+
+    // ==== for all anchors ====
+    for(var i = 0; i < anchor_list.length; i++) {
+        // Get the anchor
+        curr_anchor = data.nodes.get(anchor_list[i])
+
+        // Get the informations, like a node
+        exported_anchor = export_node(curr_anchor)
+
+        // For all nodes, we check the existence of a link to the anchor
+        exported_anchor.members = []
+        for(var j = 0; j < node_id_list.length; j++) {
+
+            // If the current anchor id is not the current node id (no link from anchor to itself)
+            if(anchor_list[i] !== node_id_list[j]){
+                // Get the list of edges between current anchor and current node
+                common_edges = get_edge_between_nodes_two_ways(node_id_list[j], anchor_list[i])
+
+                // If the current node is linked to the anchor
+                if(common_edges.length !== 0){
+                    // We add the node to the class
+                    exported_anchor.members.push(node_id_list[j])
+                }
+            }
+        }
+
+        clusters.push(exported_anchor)
+    }
+
+    return clusters
+}
+
+/*
+'meta': {'source': 'DBDUMP'},
+*/
+function get_meta(){
+    meta = {}
+    meta.source = "VISJS"
+    return meta
+}
+
 // ================ UTILITIES ================
 function graph_to_JSON() {
         var nodeData = [];
@@ -65,15 +219,19 @@ function graph_to_JSON() {
 	}
 
 function classes_to_JSON() {
+    // Construct a json representation of nodes and clusters
     var classes = [];
 
+    // Get list of all nodes (by their ids)
     var node_id_list = data.nodes.getIds()
 
+    // ==== for all anchors ====
     for(var i = 0; i < anchor_list.length; i++) {
         //TODO : Problem here !
+        // get the anchor information (label ..)
         tmp_class = {}
         tmp_class.label = data.nodes.get(anchor_list[i]).label
-        tmp_class.member_list = []
+        tmp_class.members = []
         counter = 0
 
         // For all nodes that could be links to the anchor
@@ -88,7 +246,7 @@ function classes_to_JSON() {
                 // If the current node is link to the anchor
                 if(common_edges.length !== 0){
                         // We add the node to the class
-                        tmp_class.member_list.push(node_id_list[j])
+                        tmp_class.members.push(node_id_list[j])
                         counter += 1
                 }
             }
@@ -97,10 +255,58 @@ function classes_to_JSON() {
         classes.push(tmp_class)
     }
 
+    // ==== For all nodes, add them ====
+    var nodeData = [];
+
+    // Add nodes
+    data.nodes.get().forEach(function(nodeD) {
+        tmp_node = {}
+        // Add attributes of a node to the new list
+        tmp_node.id = nodeD.id
+        tmp_node.shape = nodeD.shape
+        tmp_node.image = nodeD.image
+
+        if (nodeD.group !== null){
+            tmp_node.group = nodeD.group
+        }
+        if (nodeD.label !== null){
+            tmp_node.label = nodeD.label
+        }
+
+        // Store the node in the list to be exported
+        nodeData.push(tmp_node);
+	});
+
     console.log("Classes sorting to export : ")
     console.log(classes)
 
-    var jsonData = JSON.stringify(classes);
+    console.log("Nodes to export : ")
+    console.log(nodeData)
+
+    // Merge everything
+    final = {}
+    final.classes = classes
+    final.nodes = nodeData
+
+    var jsonData = JSON.stringify(final);
+    return jsonData;
+}
+
+function graphe_correct_to_json(){
+    // Fetch data
+    nodes = get_nodes(false) //Not with clusters
+    edges = get_edges()
+    clusters = get_clusters()
+    meta = get_meta()
+
+    // Merge everything
+    final = {}
+    final.meta = meta
+    final.clusters = clusters
+    final.nodes = nodes
+    final.edges = edges
+
+    var jsonData = JSON.stringify(final);
     return jsonData;
 }
 
